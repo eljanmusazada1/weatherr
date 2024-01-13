fe867e4ee6ba1c95ae5fd5037ef2fa779a8dec5f
eljanmusazada1 <138122027+eljanmusazada1@users.noreply.github.com>
2024-01-13 22:26:00 +0400

    Add files via upload

---
+++ Weather-main/app.js
@@ -0,0 +1,38 @@
+"use strict";
+$(document).ready(function () {
+    
+    
+    
+    
+    $(document).on("keypress", function (e) {
+        if (e.which == 13) {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=99f9d6cec44daf9e42341c9b70439bff&units=metric`;
+            async function myweather() {
+                let responce = await fetch(url);
+                let data = await responce.json();
+                $("#city").html(data.name);
+                $("#temp").html(data.main.temp);
+                $("#feels").html(data.main.feels_like);
+                $("#myWeather").val("");
+                console.log(data);
+            }
+            myweather();
+        }
+    });
+
+
+    $(".btn").click(function (e) {
+        e.preventDefault();
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myWeather").val()}&appid=99f9d6cec44daf9e42341c9b70439bff&units=metric`;
+        async function myweather() {
+            let responce = await fetch(url);
+            let data = await responce.json();
+            $("#city").html(data.name);
+            $("#temp").html(data.main.temp);
+            $("#feels").html(data.main.feels_like);
+            $("#myWeather").val("");
+            console.log(data);
+        }
+        myweather();
+    });
+});
